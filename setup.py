faed05a3ae62e5b18e4d4d4de194b84388db6e04
Amirsina Torfi <astorfi@users.noreply.github.com>
2017-04-06 15:14:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='speech_feature_extraction',
+setup(name='speechpy',
       version='0.1',
       description='The funniest joke in the world',
       author='asd',
       author_email='amirsina.torfi@gmail.com',
       license='MIT',
-      packages=['speech_feature_extraction'],
+      packages=['speechpy'],
       install_requires=[
           'scipy',
           'numpy',
