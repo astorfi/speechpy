dd70966a7bf22083a264ab916901baa99e7277cb
Amirsina Torfi <amirsina.torfi@gmail.com>
2017-04-06 23:57:48 -0400

    adding tests

---
+++ tests/test_advanced.py
@@ -0,0 +1,14 @@
+from .context import speechpy
+
+import unittest
+
+
+class AdvancedTestSuite(unittest.TestCase):
+    """Advanced test cases."""
+
+    def test_thoughts(self):
+        self.assertIsNone(speechpy.mfcc())
+
+
+if __name__ == '__main__':
+    unittest.main()
