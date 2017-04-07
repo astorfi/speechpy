13851912eaedc53fa0715280813c4f7aaabd9cee
Amirsina Torfi <astorfi@users.noreply.github.com>
2017-04-07 00:32:13 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -7,13 +7,13 @@ fs, signal = wav.read(file_name)
 signal = signal[:,0]
 
 ############# Extract MFCC features #############
-mfcc = speechpy.mfcc_feature(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
+mfcc = speechpy.mfcc(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
              num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
 mfcc_feature_cube = speechpy.extract_derivative_feature(mfcc)
 print('mfcc feature cube shape=', mfcc_feature_cube.shape)
 
 ############# Extract logenergy features #############
-logenergy = speechpy.logenergy_feature(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
+logenergy = speechpy.mfe(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
              num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
 logenergy_feature_cube = speechpy.extract_derivative_feature(logenergy)
 print('logenergy features=', logenergy.shape)
