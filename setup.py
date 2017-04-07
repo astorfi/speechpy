acf70a190e0fc50ac4fc5c200008d2d862c82cd6
Amirsina Torfi <astorfi@users.noreply.github.com>
2017-04-07 00:36:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE.rst') as f:
     license = f.read()
 
 setup(name='speechpy',
