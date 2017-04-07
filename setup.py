8ab54425812c9a953cc64b817c0e1b6b8de08bbf
Amirsina Torfi <astorfi@users.noreply.github.com>
2017-04-07 00:35:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 with open('README.rst') as f:
-    readme = f.read()
+    readme_file = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    license_file = f.read()
 
 setup(name='speechpy',
       version='0.1',
       description='The python package for extracting speech features.',
-      long_description=readme,
+      long_description=readme_file,
       author='Amirsina Torfi',
       author_email='amirsina.torfi@gmail.com',
       license='MIT',
       url='https://github.com/astorfi/speech_feature_extraction',
-      license=license,
+      license=license_file,
       packages=find_packages(exclude=('tests', 'docs')),
       install_requires=[
           'scipy',
