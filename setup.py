303d42efe97c9008cbe1b1a51d30352d0a739b55
iramirsina <amirsina.torfi@gmail.com>
2017-06-14 20:38:24 -0400

    Testing Travis CI

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 from setuptools import setup, find_packages
 
-# with open('README.rst') as f:
-#     readme = f.read()
-
-# with open('LICENSE.rst') as f:
-#     license = f.read()
-
 setup(name='speechpy',
-      version='0.1',
+      version='0.2',
       description='The python package for extracting speech features.',
-#       long_description=readme,
       author='Amirsina Torfi',
       author_email='amirsina.torfi@gmail.com',
       url='https://github.com/astorfi/speech_feature_extraction',
-#       license=license,
       packages=find_packages(exclude=('tests', 'docs')),
       include_package_data=True,
       install_requires=[
