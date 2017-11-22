38e17ae2c8ebd40a2946254c479150e96fb43992
iramirsina <amirsina.torfi@gmail.com>
2017-11-22 18:36:42 -0500

    read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,14 +121,7 @@ html_logo = '_static/img/speechpy_logo.gif'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# html_style_path = 'css/pytorch_theme.css'
-html_context = {
-    'css_files': [
-        'https://fonts.googleapis.com/css?family=Lato',
-        '_static/css/pytorch_theme.css'
-    ],
-}
-
+html_style_path = 'css/pytorch_theme.css'
 
 # -- Options for HTMLHelp output ------------------------------------------
 
--- docs/source/conf.py~
+++ docs/source/conf.py~
@@ -121,6 +121,14 @@ html_logo = '_static/img/speechpy_logo.gif'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# html_style_path = 'css/pytorch_theme.css'
+html_context = {
+    'css_files': [
+        'https://fonts.googleapis.com/css?family=Lato',
+        '_static/css/pytorch_theme.css'
+    ],
+}
+
 
 # -- Options for HTMLHelp output ------------------------------------------
 
