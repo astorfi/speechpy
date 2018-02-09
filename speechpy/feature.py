2cbc5935390978004a4b08e179e3ac33f02b6201
Matthew D. Scholefield <matthew3311999@gmail.com>
2018-02-08 22:34:14 -0600

    Speed improvements  - Add function caching to speed up computation of functions called with  the same parameters  - Remove use of np.round since it's slower for primitives

--- speechpy/feature.py
+++ speechpy/feature.py
@@ -1,11 +1,13 @@
 from __future__ import division
+
 import numpy as np
 from . import processing
 from scipy.fftpack import dct
-import math
+from functools import lru_cache
 from . import functions
 
 
+@lru_cache()
 def filterbanks(num_filter, fftpoints, sampling_freq, low_freq=None, high_freq=None):
     """Compute the Mel-filterbanks. Each filter will be stored in one rows. The columns correspond to fft bins.
 
