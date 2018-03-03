fcd4079349dac61396a2f1afa5fa4e3e387ac98c
Amirsina Torfi <amirsina.torfi@gmail.com>
2018-03-03 00:56:18 -0500

    test local added to travis

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='speechpy',
       author='Amirsina Torfi',
       author_email='amirsina.torfi@gmail.com',
       url='https://github.com/astorfi/speechpyn',
+      download_url = 'https://github.com/astorfi/speechpy/archive/2.2.zip',
       packages=find_packages(exclude=('tests', 'docs')),
       include_package_data=True,
       install_requires=[
