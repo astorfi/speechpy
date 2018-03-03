d6e3107adc8241f930fee7e660a77ca10a933854
Amirsina Torfi <amirsina.torfi@gmail.com>
2018-03-03 00:52:43 -0500

    test local added to travis

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='speechpy',
-      version='2.1',
+      version='2.2',
       description='The python package for extracting speech features.',
       author='Amirsina Torfi',
       author_email='amirsina.torfi@gmail.com',
-      url='https://github.com/astorfi/speech_feature_extraction',
+      url='https://github.com/astorfi/speechpyn',
       packages=find_packages(exclude=('tests', 'docs')),
       include_package_data=True,
       install_requires=[
