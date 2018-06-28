9fa9013dae23340f69e385bae62c978f89380c2c
Amirsina Torfi <amirsina.torfi@gmail.com>
2018-06-28 13:10:42 -0400

    add references

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -354,7 +354,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/', None),
+    'python': ('https://docs.python.org/3/', None),
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
 }
 
